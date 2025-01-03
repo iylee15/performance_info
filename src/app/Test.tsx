00294403f133bc0fd081bb5f6f4e22b747fa44a6
iylee <iylee153@gmail.com>
2025-01-03 16:21:32 +0900

    url 수정정

--- src/app/Test.tsx
+++ src/app/Test.tsx
@@ -15,7 +15,7 @@ export default function Test() {
     
         const allPerformanceData = [];
         for (let i = 1; i <= 10; i++){
-            const response = await axios.get(`https://kopis.or.kr/openApi/restful/pblprfr?service=ffb2e5d6f87f41ca9e2a3314ff44adad&stdate=20230401&eddate=20230430&cpage=${i}&rows=10&signgucode=11&signgucodesub=1111`);
+            const response = await axios.get(`https://kopis.or.kr/openApi/restful/pblprfr?service=${serviceKey}&stdate=20230401&eddate=20230430&cpage=${i}&rows=10&signgucode=11&signgucodesub=1111`);
             allPerformanceData.push({...response.data});
         }
         // const { apiData } = await axios.get(url);
